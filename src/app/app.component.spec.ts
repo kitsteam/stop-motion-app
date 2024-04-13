40a195ee881652fbbfa2e6f0109799de4af8f4c4
nwittstruck <nwittstruck@users.noreply.github.com>
2024-04-13 15:45:56 +0200

    chore: add caching step: test ci failure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,7 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+    expect(false).toBeTruthy();
   });
-  // TODO: add more tests!
-
 });
