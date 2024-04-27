f828d77336a7d3cafad9262aa730339d801a82ce
nwittstruck <nwittstruck@users.noreply.github.com>
2024-04-27 15:27:22 +0200

    bug: removing image does not remove it from final video #35: don't limit tests

--- src/app/services/animator/animator.service.spec.ts
+++ src/app/services/animator/animator.service.spec.ts
@@ -4,7 +4,7 @@ import { TestBed } from '@angular/core/testing';
 import { AnimatorService } from './animator.service';
 import { TranslateModule } from '@ngx-translate/core';
 
-fdescribe('AnimatorService', () => {
+describe('AnimatorService', () => {
   let service: AnimatorService;
 
   beforeEach(() => {
