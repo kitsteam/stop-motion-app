2b668caa1a3735eeb8b41b534eb9e11daf183a3a
Nicholas <nwittstruck@users.noreply.github.com>
2025-09-27 18:36:27 +0200

    chore: upgrade translate (#440)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouteReuseStrategy } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { HttpClient, HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 // Modules and Components
 import { environment } from '@environment/environment';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,28 +15,29 @@ import { ComponentsModule } from '@components/components.module';
 import { PipesModule } from '@pipes/pipes.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-export const translationLoaderFactory = (http: HttpClient) => new TranslateHttpLoader(http);
-@NgModule({ declarations: [AppComponent],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        IonicModule.forRoot({
-            mode: 'md' // force material UI mode, iOS has not been styled
-        }),
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        ComponentsModule,
-        PipesModule,
-        TranslateModule.forRoot({
-            loader: { provide: TranslateLoader, useFactory: translationLoaderFactory, deps: [HttpClient] }
-        }),
-        ServiceWorkerModule.register('ngsw-worker.js', {
-            enabled: environment.production,
-            // Register the ServiceWorker as soon as the app is stable
-            // or after 30 seconds (whichever comes first).
-            registrationStrategy: 'registerWhenStable:30000'
-        }),
-        NgbModule], providers: [
-        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-        provideHttpClient(withInterceptorsFromDi()),
-    ] })
+@NgModule({
+  declarations: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent], imports: [BrowserModule,
+    IonicModule.forRoot({
+      mode: 'md' // force material UI mode, iOS has not been styled
+    }),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ComponentsModule,
+    PipesModule,
+    TranslateModule.forRoot({
+      loader: provideTranslateHttpLoader()
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    NgbModule], providers: [
+      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+      provideHttpClient(withInterceptorsFromDi()),
+    ]
+})
 export class AppModule { }
